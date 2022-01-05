9861bc3d0c3a9d4731ea9a9f62cb1e065b6882da
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-05 11:29:42 +0200

    Added connection with commerce

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
@@ -6,6 +7,20 @@ import React from 'react';
 import { Products, Navbar} from './components';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    console.log(products);
+
     return (
         <div>
             <Navbar />
