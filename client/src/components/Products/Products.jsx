c90ad47ab08798b70dd65a048ee3f5f1319f0b59
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-05 09:34:41 +0200

    Added css products

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
-import Product from './Product/Product'
+import Product from './Product/Product';
+
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5', image: 'https://www.sportsdirect.com/images/marketing/btq-run-mens-run-shoes-500x356.jpg' },
@@ -9,9 +11,12 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
 
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify='center' spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
