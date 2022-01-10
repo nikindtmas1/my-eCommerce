c86c34bb240c420df9996cd62c2425c9e88a817a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-10 12:50:34 +0200

    Added Link in Cart component

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
+import { Link } from 'react-router-dom';
+
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
@@ -10,7 +12,8 @@ const Cart = ({ cartData }) => {
     const classes = useStyles();
 
     const EmptyCart = () => {
-        <Typography variant='subtitle1' >You have no items in your shopping cart, start adding same!</Typography>
+        <Typography variant='subtitle1' >You have no items in your shopping cart, start adding same!
+        </Typography>
     };
 
     const FilledCart = () => {
