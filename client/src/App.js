6f39e862481325a9c239c2d71ef86b6f585a9805
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-10 10:28:10 +0200

    Change logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar,  } from './components';
+import { Products, Navbar, Cart } from './components';
 
 const App = () => {
     const [products, setProducts] = useState([]);
@@ -37,7 +37,7 @@ const App = () => {
         <div>
             <Navbar totalItems={cart.total_items} />
             <Products products={products} onAddToCart={handleAddToCart} />
-            {/* <Cart cart={cart}/> */}
+            <Cart cartData={cart} />
         </div>
     )
 }
--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,18 +3,19 @@ import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
-    const isEmpty = cart.line_items.length === 0;
+const Cart = ({ cartData }) => {
+    console.log(cartData);
+    //const isEmpty = !cartData.line_items.length;
     const classes = useStyles();
 
     const EmptyCart = () => {
-        <Typography variant='subtitle1' >You have no items inyour shopping cart, start adding same!</Typography>
+        <Typography variant='subtitle1' >You have no items in your shopping cart, start adding same!</Typography>
     };
 
     const FilledCart = () => {
         <>
         <Grid container spacing={3}>
-            {cart.line_items.map((item) => (
+            {cartData.line_items.map((item) => (
                 <Grid item xs={12} sm={4} key={item.id}>
                     <div>{item.name}</div>
                 </Grid>
@@ -22,7 +23,7 @@ const Cart = ({ cart }) => {
         </Grid>
         <div className={classes.cardDetails}>
                 <Typography variant='h4'>
-                    Subtotal: {cart.subtotal.formatted_with_symbol}
+                    Subtotal: {cartData.subtotal.formatted_with_symbol}
                 </Typography>
                 <div>
                     <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
@@ -32,11 +33,13 @@ const Cart = ({ cart }) => {
         </>
     };
 
+    if(!cartData.line_items) return 'Loading...';
+
     return (
         <Container>
             <div className={classes.toolbar} />
             <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
-            {isEmpty ? <EmptyCart /> : <FilledCart />}
+            {!cartData.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
 }
