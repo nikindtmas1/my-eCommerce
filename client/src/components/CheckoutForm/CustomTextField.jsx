78f8be89dfab04b413b4e0013b51931a7cf115e5
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-11 13:31:59 +0200

    Change form logic

--- client/src/components/CheckoutForm/CustomTextField.jsx
+++ client/src/components/CheckoutForm/CustomTextField.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { TextField, Grid } from '@material-ui/core';
+import { useFormContext, Controller } from 'react-hook-form';
 
-const CustomTextField = () => {
+const FormInput = ({ name, label, required }) => {
+    const { control } = useFormContext();
     return (
-        <div>
+        <Grid item xs={12} sm={6}>
+            <TextField control={control}
+            fullWidth
+            name={name}
+            label={label}
+            required={required}
+            />
             
-        </div>
+            {/* <Controller
+            as={TextField}
+            control={control}
+            fullWidth
+            name={name}
+            label={label}
+            required={required}
+            /> */}
+        </Grid>
     )
 }
 
-export default CustomTextField
+export default FormInput;
