dea272d5b4bacdb1c625f77cbcb8eae9fb9985a3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-11 11:25:59 +0200

    Create Checkout component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
 
-     const [cart, setCart] = useState({});
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -24,25 +24,25 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const {cart} = await commerce.cart.add(productId, quantity);
+        const { cart } = await commerce.cart.add(productId, quantity);
 
         setCart(cart)
     };
 
     const handleUpdateCartQty = async (productId, quantity) => {
-        const {cart} = await commerce.cart.update(productId, {quantity});
+        const { cart } = await commerce.cart.update(productId, { quantity });
 
         setCart(cart)
     };
 
     const handleRemoveFromCart = async (productId) => {
-        const {cart} = await commerce.cart.remove(productId);
+        const { cart } = await commerce.cart.remove(productId);
 
         setCart(cart);
     };
 
     const handleEmptyCart = async () => {
-        const {cart} = await commerce.cart.empty();
+        const { cart } = await commerce.cart.empty();
 
         setCart(cart);
     }
@@ -52,7 +52,7 @@ const App = () => {
         fetchCart();
     }, []);
 
-    
+
 
     return (
         <Router>
@@ -64,11 +64,14 @@ const App = () => {
                     </Route>
 
                     <Route exact path='/cart'>
-                        <Cart cartData={cart} 
-                        handleUpdateCartQty={handleUpdateCartQty}
-                        handleRemoveFromCart={handleRemoveFromCart}
-                        handleEmptyCart ={handleEmptyCart}
+                        <Cart cartData={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
                         />
+                    </Route>
+                    <Route exact path='/checkout' >
+                        <Checkout />
                     </Route>
                 </Switch>
             </div>
--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -35,7 +35,7 @@ const Cart = ({ cartData, handleUpdateCartQty, handleRemoveFromCart, handleEmpty
                 </Typography>
                 <div>
                     <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
-                    <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
+                    <Button component={Link} to='/checkout' className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
                 </div>
         </div>
         </>
