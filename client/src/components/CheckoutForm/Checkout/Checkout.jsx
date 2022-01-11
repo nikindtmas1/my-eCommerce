e6a0669d5e2753be23d29bd9d8ef2951093e2da6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-11 12:14:30 +0200

    Added logic in checkout component

--- client/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ client/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,16 +1,24 @@
 import React, {useState} from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
     const classes = useStyles();
-    const [activeStep, setActiveStep] = useState(0);
+    const [activeStep, setActiveStep] = useState(2);
 
     const Form = () => activeStep === 0 
     ? <AddressForm />
     : <PaymentForm />
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    );
 
     return (
         <>
