bdf06de7038aa8325631e8e2694f45b8cfc5b146
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-17 11:39:04 +0200

    Import modules

--- client/src/components/CheckoutForm/Checkout/Review.jsx
+++ client/src/components/CheckoutForm/Checkout/Review.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Typography, List, ListItem, ListItemText  } from '@material-ui/core';
 
 const Review = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Typography variant='h4' gutterBottom>Order summary</Typography>
+        </>
     )
 }
 
